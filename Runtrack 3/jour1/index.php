7ab25bfdd439566a48c6198bf59d34888962ad7e
arilas-rouaiguia <71872334+arilas-rouaiguia@users.noreply.github.com>
2021-03-08 15:39:16 +0100

    Add files via upload

---
+++ Runtrack 3/jour1/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+   <div class="popup" onclick="myFunction()">Click me!
+  <span class="popuptext" id="myPopup">Popup text...</span>
+</div> 
+</head>
+<body>
+<ul id="Waffle">
+	<li>Je suis Vide</li>
+</ul>
+</body>
+	<script type="text/javascript" src="script.js"> </script>
+</html>
