d261188ca33a1b37246d68be255a5cc93651c83a
arilas-rouaiguia <71872334+arilas-rouaiguia@users.noreply.github.com>
2021-03-09 11:07:01 +0100

    Add files via upload

---
+++ Jour02/job02/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+</head>
+<body>
+<div id="show">
+<article>
+“L'important n'est pas la chute, mais l'atterrissage.”.
+</article>
+</div>
+<button onclick="showhide()">Try it</button>
+</body>
+	<script type="text/javascript" src="script.js"> </script>
+</html>
