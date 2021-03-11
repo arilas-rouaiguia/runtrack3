5917b93bc778a0abe040fd922c9d78b672796607
arilas-rouaiguia <71872334+arilas-rouaiguia@users.noreply.github.com>
2021-03-11 11:42:47 +0100

    Add files via upload

---
+++ Jour04/job1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Test</title>
+    <meta charset="UTF-8" />
+
+</head>
+ 
+<body>
+<div id="demo">
+<button type="button" onclick="Oui()">Change Content</button>
+</div>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.0/jquery.min.js"></script>
+	<script src="script.js"></script>
+</body>
+ 
+</html>
