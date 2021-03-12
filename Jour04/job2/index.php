0431549dbfe8baf2fbb5ef8a6ef7b0e9d854b34a
arilas-rouaiguia <71872334+arilas-rouaiguia@users.noreply.github.com>
2021-03-12 11:38:58 +0100

    Add files via upload

---
+++ Jour04/job2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Test</title>
+    <meta charset="UTF-8" />
+
+</head>
+ 
+<body>
+<p id="demo"></p>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.0/jquery.min.js"></script>
+	<script src="script.js"></script>
+</body>
+ 
+</html>
---
+++ Jour04/job2/script.js
@@ -0,0 +1,3 @@
+myObj = {"name": "La Plateforme_", "address": "8 rue hozier", "city": "Marseille", "nb_staff": "11" ,"creation":"2019"};
+x = myObj.city;
+document.getElementById("demo").innerHTML = x;
